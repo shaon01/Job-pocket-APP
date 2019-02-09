bb8a4ad395c213181ffc4e01550f1fe84e50c057
shaon01 <shaoun46@gmail.com>
2019-02-09 20:22:46 +0100

    Added functionality to update metor description from a text file. need update of text field itself

--- app/src/main/java/com/example/jobpocket/Mentor.java
+++ app/src/main/java/com/example/jobpocket/Mentor.java
@@ -1,11 +1,21 @@
 package com.example.jobpocket;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+//import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+//import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class Mentor extends AppCompatActivity {
 
@@ -24,6 +34,40 @@ public class Mentor extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // START reading mentor description from text file
+
+        StringBuilder tempStory = new StringBuilder();
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(getAssets().open("mentorStory.txt")));
+
+            // do reading, usually loop until end of file reading
+            String mLine;
+            while ((mLine = reader.readLine()) != null) {
+                tempStory.append(mLine);
+                tempStory.append('\n');
+
+            }
+        } catch (IOException e) {
+            //log the exception
+            Toast.makeText(getApplicationContext(),"Error reading file!", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    //log the exception
+                }
+            }
+        }
+        TextView story = findViewById(R.id.mentorStory);
+        story.setText(tempStory) ;
+
+        // END reading mentor description from text file
+
     }
 
 }
