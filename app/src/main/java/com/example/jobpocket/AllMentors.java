1841549115c99962694048bfe713a38b4d21741c
shaon01 <shaoun46@gmail.com>
2019-02-10 22:34:22 +0100

    initial addition of recycle view

--- app/src/main/java/com/example/jobpocket/AllMentors.java
+++ app/src/main/java/com/example/jobpocket/AllMentors.java
@@ -4,10 +4,19 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AllMentors extends AppCompatActivity {
+
+
+    private List<Person> persons;
+    private RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +24,15 @@ public class AllMentors extends AppCompatActivity {
         setContentView(R.layout.activity_all_mentors);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        rv=(RecyclerView)findViewById(R.id.allMentorViewer);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
+        rv.setHasFixedSize(true);
+
+        initializeData();
+        initializeAdapter();
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +42,18 @@ public class AllMentors extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void initializeData(){
+        persons = new ArrayList<>();
+        persons.add(new Person("Emma Wilson", "23 years old", R.drawable.emma));
+        persons.add(new Person("Lavery Maiss", "25 years old", R.drawable.lavery));
+        persons.add(new Person("Lillie Watts", "35 years old", R.drawable.lillie));
+    }
+
+    private void initializeAdapter(){
+        RVAdapter adapter = new RVAdapter(persons);
+        rv.setAdapter(adapter);
     }
 
 }
--- app/src/main/java/com/example/jobpocket/AllMentorsFragment.java
+++ app/src/main/java/com/example/jobpocket/AllMentorsFragment.java
@@ -2,9 +2,12 @@ package com.example.jobpocket;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -14,9 +17,15 @@ public class AllMentorsFragment extends Fragment {
     public AllMentorsFragment() {
     }
 
+    private List<Person> persons;
+    private RecyclerView rv;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_all_mentors, container, false);
     }
+
+
 }
